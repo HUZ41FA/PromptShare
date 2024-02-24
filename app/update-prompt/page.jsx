28ad95fb9252e6679f4fe7cc45c49f74c8636ea0
huzaifa <huzaifakhanned@gmail.com>
2024-02-24 19:33:10 +0500

    issue resolved with useSearchParams()

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useSession } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Form from "@components/Form";
 
